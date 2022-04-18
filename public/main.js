7d3f5f3f4809ba21a0897b9ba1a86c21d2639ab6
Prabudh Mishra <prabudh.mishra18@gmail.com>
2022-04-18 08:29:45 +0530

    Created demo app for electron with react

---
+++ public/main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
+const isDev = require("electron-is-dev");
+
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    },
+  });
+
+  ipcMain.on("app:get-app-path", (event) => {
+    event.returnValue = app.getAppPath();
+  });
+
+  win.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+}
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
+
+app.on("ready", createWindow);
